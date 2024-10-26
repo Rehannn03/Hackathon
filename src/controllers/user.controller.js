7b8c05b9d95e8ec19e18aeae1c6c50f179235a8f
REHAN SHAH <spamhomeacc123@gmail.com>
2024-10-26 15:11:56 +0530

    some changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,3 +2,21 @@ import User from "../model/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const login=asyncHandler(async(req,res)=>{
+    const {email,password}=req.body
+    const user=await User.findOne({email})
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const isMatch=await user.isPasswordCorrect(password)
+
+    if(!isMatch){
+        throw new ApiError(400,"Invalid credentials")
+    }
+
+    const token=await user.generateAccessToken()
+})
