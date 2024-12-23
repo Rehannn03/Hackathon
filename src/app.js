de90c06088ad43b52b9e15f1efdc14dd92e73297
rsayyed591 <rehansayyed591@gmail.com>
2024-12-24 01:33:19 +0530

    updated cors in api.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,25 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app=express()
+const allowedOrigins = ['http://localhost:5173', 'https://err404-manager.vercel.app']
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use(cors())
+// app.use(cors())
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true) // Allow this origin
+      } else {
+        callback(new Error('Not allowed by CORS')) // Reject other origins
+      }
+    },
+    credentials: true // Allow credentials (cookies, authorization headers)
+  })
+)
 
 import superAdminRoutes from './routes/superAdmin.routes.js'
 import adminRoutes from './routes/admin.routes.js'
