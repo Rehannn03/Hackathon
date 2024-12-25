cbff0cff689bc5c7e8acaa7a3eb162bcc2ed78dd
REHAN SHAH <spamhomeacc123@gmail.com>
2024-12-25 16:13:00 +0530

    Vercel Analytics

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-
+import {withVercelAnalytics} from '@vercel/analytics'
 const app=express()
 const allowedOrigins = ['http://localhost:5173', 'https://err404-manager.vercel.app']
 
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 // app.use(cors())
-
+app.use(withVercelAnalytics())
 app.use(
   cors({
     origin: function (origin, callback) {
